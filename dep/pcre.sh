f0f05aba8a67dcacab0182276bea32247f73fed5
timi-owo <781662377@qq.com>
2021-01-12 08:51:47 +0800

    Added automatic installation script for 'PCRE v8.44'

---
+++ dep/pcre.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/bash
+
+# specify version to install
+VERSION='8.44'
+
+# --------------------------------------------------
+
+LAUNCH_DIR="$(cd $(dirname $0); pwd)"
+
+print_red()
+{
+	printf '\033[1;31;31m%b\033[0m' "$1\n"
+}
+
+print_green()
+{
+	printf '\033[1;31;32m%b\033[0m' "$1\n"
+}
+
+print_yellow()
+{
+	printf '\033[1;31;33m%b\033[0m' "$1\n"
+}
+
+print_yellow "* installing 'pcre v${VERSION}' ···"
+
+# don't edit those variables
+DOWNLOAD_URL="https://ftp.pcre.org/pub/pcre/pcre-${VERSION}.tar.gz"
+DOWNLOAD_FILE="/tmp/pcre-${VERSION}.tar.gz"
+SOURCE_DIR="/usr/local/src/pcre-${VERSION}"
+
+if [ -d "${SOURCE_DIR}" ]; then
+	print_red "* source directory '${SOURCE_DIR}' already exists"
+	exit 1
+fi
+
+wget -q ${DOWNLOAD_URL} -O ${DOWNLOAD_FILE}
+
+if [ ! -f "${DOWNLOAD_FILE}" ]; then
+	print_red "* failed to downloading from '${DOWNLOAD_URL}'"
+	exit 1
+fi
+
+tar -xzf ${DOWNLOAD_FILE} -C /usr/local/src
+rm -f ${DOWNLOAD_FILE}
+
+if [ -d "${SOURCE_DIR}" ]; then
+
+	# install pre-compile dependencies
+	yum groupinstall -y "Development Tools"
+
+	# redirect to source directory
+	cd ${SOURCE_DIR}
+
+	# you can custom arguments whatever you want (except --prefix)
+	./configure --prefix=/usr/local --enable-utf --enable-jit
+
+	# build and install then back to previous directory
+	make && make install && cd ${LAUNCH_DIR}
+
+	# delete source directory
+	rm -rf ${SOURCE_DIR}
+
+else
+	print_red "* failed to extract files from '${DOWNLOAD_FILE}'"
+	exit 1
+fi
+
+# refresh system dynamic libraries cache
+echo "/usr/local/lib" > /etc/ld.so.conf.d/usr-local-lib.conf
+echo "/usr/local/lib64" > /etc/ld.so.conf.d/usr-local-lib64.conf
+ldconfig
+
+print_green "* installation completed"
+
+# done #
